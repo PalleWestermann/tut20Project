a348e45a93cd8e8732eaebeb65122984bf6b4a5c
Palle Westermann <pwe@tec.dk>
2025-09-29 12:04:31 +0200

    get_api_done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from "./components/user/user";
+import { Admin } from "./components/admin/admin";
+import { GetApi } from './components/get-api/get-api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GetApi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
